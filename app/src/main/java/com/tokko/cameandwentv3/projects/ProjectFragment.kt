800a0aee5c1fddb299b481ca8557b98516a666c3
tokko <andreg@kth.se>
2017-06-30 23:01:43 +0200

    project list empty view

--- app/src/main/java/com/tokko/cameandwentv3/projects/ProjectFragment.kt
+++ app/src/main/java/com/tokko/cameandwentv3/projects/ProjectFragment.kt
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.squareup.otto.Bus
 import com.tokko.cameandwentv3.MyApplication
 import com.tokko.cameandwentv3.events.EventEditProject
 import com.tokko.cameandwentv3.model.Project
@@ -18,27 +19,28 @@ class ProjectFragment : ListFragment() {
     var adapter : ArrayAdapter<Project>? = null
     var myRef : DatabaseReference? = null
     var listener : ValueEventListener? = null
-
+    var bus : Bus? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var uid = FirebaseAuth.getInstance().currentUser?.uid
         var database = FirebaseDatabase.getInstance()
         myRef = database.reference.child(uid).child("projects")
-
-       listener = object : ValueEventListener {
+        bus = (activity.application as MyApplication).bus
+        listener = object : ValueEventListener {
            override fun onDataChange(p0: DataSnapshot?) {
                var projects = p0?.getValue(object: GenericTypeIndicator<HashMap<@kotlin.jvm.JvmSuppressWildcards String, @kotlin.jvm.JvmSuppressWildcards Project>>(){ })
                if(projects != null) {
                    adapter = ArrayAdapter<Project>(activity, android.R.layout.simple_list_item_1, android.R.id.text1, projects.values.toList())
                    listAdapter = adapter
                    adapter!!.notifyDataSetChanged()
+                   add_project.visibility = View.VISIBLE
                }
            }
 
            override fun onCancelled(p0: DatabaseError?) {
                Toast.makeText(context, "Cancelled", Toast.LENGTH_SHORT).show()
            }
-       }
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -48,17 +50,19 @@ class ProjectFragment : ListFragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         add_project.setOnClickListener { (activity.application as MyApplication).bus.post(EventEditProject(null)) }
+        list.setOnItemClickListener({_, _, position, _ -> bus!!.post(EventEditProject(adapter!!.getItem(position)))})
+        list.emptyView = list_empty
     }
 
     override fun onStart() {
         super.onStart()
         myRef?.addValueEventListener(listener)
-        (activity.application as MyApplication).bus.register(this)
+        bus!!.register(this)
     }
 
     override fun onStop() {
         super.onStop()
         myRef?.removeEventListener(listener)
-        (activity.application as MyApplication).bus.unregister(this)
+        bus!!.unregister(this)
     }
 }
