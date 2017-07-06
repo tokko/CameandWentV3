885cffce2ed204946662997a88052d47c91100c5
tokko <andreg@kth.se>
2017-07-06 20:08:54 +0200

    geofencing works i guess?

--- app/src/main/java/com/tokko/cameandwentv3/geofence/GeofenceService.kt
+++ app/src/main/java/com/tokko/cameandwentv3/geofence/GeofenceService.kt
@@ -6,6 +6,7 @@ import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import com.google.android.gms.location.*
@@ -15,46 +16,30 @@ import com.tokko.cameandwentv3.model.LogEntry
 import com.tokko.cameandwentv3.model.Project
 import com.tokko.cameandwentv3.util.PermissionActivity
 import java.util.stream.Collectors
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.GoogleApiClient
+
+
 
 
 /**
  * Created by andreas on 4/07/17.
  */
-class GeofenceService : IntentService("GeofenceService") {
-
-
+class GeofenceService : IntentService("GeofenceService"), GoogleApiClient.ConnectionCallbacks {
     companion object {
         val ACTION_REGISTER = "com.tokko.cameandwentv3.geofenceservice.ACTION_REGISTER"
-        val ACTION_REGISTER_PAYLOAD_DELIVERED = "com.tokko.cameandwentv3.geofenceservice.ACTION_REGISTER_PAYLOAD_DELIVERED"
-        val EXTRA_PROJECTS = "com.tokko.cameandwentv3.geofenceservice.EXTRA_PROJECTS"
         fun initGeofences(context: Context){
             context.startService(Intent(context, GeofenceService::class.java).setAction(ACTION_REGISTER))
         }
     }
+    var client: GoogleApiClient? = null
 
     override fun onHandleIntent(intent: Intent?) {
         if(intent?.action.equals(ACTION_REGISTER)) {
-            FirebaseDatabase.getInstance().reference.child(FirebaseAuth.getInstance().currentUser!!.uid).child("projects").
-                    addValueEventListener(object : ValueEventListener {
-                        override fun onCancelled(p0: DatabaseError?) {
-
-                        }
-
-                        override fun onDataChange(p0: DataSnapshot?) {
-                            val projects = p0?.getValue(object : GenericTypeIndicator<HashMap<@JvmSuppressWildcards String, @JvmSuppressWildcards Project>>() {})
-                            if (projects != null) {
-                                val intent = Intent(this@GeofenceService, GeofenceService::class.java).setAction(ACTION_REGISTER_PAYLOAD_DELIVERED)
-                                intent.putExtra(EXTRA_PROJECTS, projects)
-                                startService(intent)
-                            } else {
-
-                            }
-                        }
-                    })
-        }
-        else if(intent?.action.equals(ACTION_REGISTER_PAYLOAD_DELIVERED)) {
-            val projects = intent?.getSerializableExtra(EXTRA_PROJECTS) as HashMap<String, Project>
-            registerGeofences(projects.values)
+            client = GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this).build()
+            client?.connect()
         }
         else{
             val geofencingEvent = GeofencingEvent.fromIntent(intent)
@@ -77,39 +62,60 @@ class GeofenceService : IntentService("GeofenceService") {
         }
     }
 
-    private fun registerGeofences(projects: Collection<Project>) {
+    override fun onConnected(p0: Bundle?) {
+        registerGeofences()
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+    }
+
+    private fun registerGeofences() {
         unRegisterGeofences()
-        val geofences = projects.stream().flatMap { p ->
-            p.locations.stream().map { l ->
-                Geofence.Builder()
-                        .setRequestId(p.id + ":" + l.id + ":" + p.title)
-                        .setCircularRegion(l.latitude, l.longitude, 200F)
-                        .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
-                        .setExpirationDuration(Geofence.NEVER_EXPIRE)
-                        //.setLoiteringDelay(1000*60*5)
-                        .build()
-            }
-        }.collect(Collectors.toList())
-        val request = GeofencingRequest.Builder().setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER).addGeofences(geofences).build()
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            val notification = Notification.Builder(this)
-                    .setContentTitle("Requires more permissions")
-                    .setContentText("This app require permissions to access your location")
-                    .setSmallIcon(R.drawable.stat_sys_warning)
-                    .setContentIntent(PendingIntent.getActivity(this, 0, Intent(this, PermissionActivity::class.java), 0))
-                    .setAutoCancel(true)
-                    .build()
-            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).notify(0, notification)
-            return
-        }
-        GeofencingClient(this).addGeofences(request, pendingIntent())
+        FirebaseDatabase.getInstance().reference.child(FirebaseAuth.getInstance().currentUser!!.uid).child("projects").
+                addValueEventListener(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError?) {
+
+                    }
+
+                    override fun onDataChange(p0: DataSnapshot?) {
+                        val projects = p0?.getValue(object : GenericTypeIndicator<HashMap<@JvmSuppressWildcards String, @JvmSuppressWildcards Project>>() {})
+                        if (projects != null) {
+                            val geofences = projects.values.stream().flatMap { p ->
+                                p.locations.stream().map { l ->
+                                    Geofence.Builder()
+                                            .setRequestId(p.id + ":" + l.id + ":" + p.title)
+                                            .setCircularRegion(l.latitude, l.longitude, 100F)
+                                            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER + Geofence.GEOFENCE_TRANSITION_EXIT)
+                                            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                                            //.setLoiteringDelay(1000*60*5)
+                                            .build()
+                                }
+                            }.collect(Collectors.toList())
+                            val request = GeofencingRequest.Builder().setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER).addGeofences(geofences).build()
+                            if (ContextCompat.checkSelfPermission(this@GeofenceService, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                                    || ContextCompat.checkSelfPermission(this@GeofenceService, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                val notification = Notification.Builder(this@GeofenceService)
+                                        .setContentTitle("Requires more permissions")
+                                        .setContentText("This app require permissions to access your location")
+                                        .setSmallIcon(R.drawable.stat_sys_warning)
+                                        .setContentIntent(PendingIntent.getActivity(this@GeofenceService, 0, Intent(this@GeofenceService, PermissionActivity::class.java), 0))
+                                        .setAutoCancel(true)
+                                        .build()
+                                (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).notify(0, notification)
+                                return
+                            }
+                            LocationServices.GeofencingApi.addGeofences(client, request, pendingIntent())
+                        } else {
+
+                        }
+                    }
+                })
 
     }
 
     private fun pendingIntent() = PendingIntent.getService(this, 0, Intent(this, GeofenceService::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
 
     private fun unRegisterGeofences(){
-        GeofencingClient(this).removeGeofences(pendingIntent())
+        LocationServices.GeofencingApi.removeGeofences(client, pendingIntent())
     }
 }
