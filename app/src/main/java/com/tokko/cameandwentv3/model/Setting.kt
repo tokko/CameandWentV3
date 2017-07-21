4dedac0f7cf4503e59336a0230f56334f2b78564
tokko <tit1rstsT/f>
2017-07-21 22:42:24 +0200

    most of automatic breaks

--- app/src/main/java/com/tokko/cameandwentv3/model/Setting.kt
+++ app/src/main/java/com/tokko/cameandwentv3/model/Setting.kt
@@ -1,21 +1,12 @@
 package com.tokko.cameandwentv3.model
 
 import android.content.Context
-import com.tokko.cameandwentv3.settings.getAutomaticBreakDuration
-import com.tokko.cameandwentv3.settings.getAutomaticBreakStart
-import com.tokko.cameandwentv3.settings.getConsultRounding
 
 /**
  * Created by andreas on 10/07/17.
  */
 class Setting {
-    var automaticBreak: Long = 30
-    var automaticBreakStart: Long = 0
+    var automaticBreak: Long = 30*60*1000
+    var automaticBreakStart: Long = 11*60*60*1000 + 30*60*1000
     var consultRounding: Boolean = true
-
-    fun init(context: Context) {
-        automaticBreak = context.getAutomaticBreakDuration()
-        automaticBreakStart = context.getAutomaticBreakStart()
-        consultRounding = context.getConsultRounding()
-    }
 }
